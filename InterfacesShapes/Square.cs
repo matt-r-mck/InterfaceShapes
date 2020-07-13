f6c20436b33e4e94256eca15b990d4dc55aaf680
matt-r-mck <matt.r.mck@gmail.com>
2020-07-12 20:59:00 -0400

    Initial Commit

--- InterfacesShapes/Square.cs
+++ InterfacesShapes/Square.cs
@@ -7,15 +7,12 @@ namespace InterfacesShapes {
 
         public int Leg1 { get; set; }
 
-
         public Square(int l1) {
             Leg1 = l1;
         }
 
         public void FindPerimeter() {
             Console.WriteLine($"Perimeter is {Leg1  * 4}");
-
         }
-
     }
 }
